8d3d240f4ccbe947fb2d7a815bdd393e7e5bf613
Vlad Chekunov <gwellir@gmail.com>
2020-11-28 19:07:54 +0300

    add some tests

---
+++ unittests/test_feed_parser.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+from parsers.feed_parser import parse_feed_title, title_compare
+from utils.db_wrapper2 import DataInterface
+from jikanpy import Jikan
+
+
+class TestFeedParser(TestCase):
+    #
+    # def __init__(self):
+    #     super().__init__()
+    #     self.di = DataInterface()
+    #     self.jikan = Jikan()
+
+    def test_name_parsing(self):
+        title1 = "[FFA] Re:Zero kara Hajimeru Isekai Seikatsu 2nd Season - 11 [1080p][HEVC][AAC].mkv"
+        self.assertEqual(parse_feed_title(title1),
+                         ('11',
+                          'FFA',
+                          'Re:Zero kara Hajimeru Isekai Seikatsu 2nd Season',
+                          1080,
+                          'mkv'))
+
+        title2 = "[Judas] Re:Zero kara Hajimeru Isekai Seikatsu - S02E11 [1080p][HEVC x265 10bit][Multi-Subs] (Weekly)"
+        self.assertEqual(parse_feed_title(title2),
+                         ('11',
+                          'Judas',
+                          'Re:Zero kara Hajimeru Isekai Seikatsu - S02',
+                          1080,
+                          'mkv'))
+
+    # def test_title_recognition(self):
+    #     params1 = ("[Judas] Re:Zero kara Hajimeru Isekai Seikatsu - S02E11 [1080p][HEVC x265 10bit][Multi-Subs] (Weekly)",
+    #                 "2020-09-16 20:16:55",
+
+    def test_title_compare(self):
+        title = 'Tonikaku Kawaii'
+        variants = [{'title': 'Haikyuu!! To the Top'}]
+
+        self.assertIsNotNone(title_compare(variants, title))
