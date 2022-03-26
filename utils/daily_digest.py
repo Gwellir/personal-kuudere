bbdecacd37d95c7ccc77b7f24a689dd8d0a24d7b
Rasifiel <laialnarrin@gmail.com>
2022-03-26 02:43:45 +0100

    Daily digest v0.1.

---
+++ utils/daily_digest.py
@@ -0,0 +1,27 @@
+import datetime
+
+import requests
+
+
+def get_digest():
+    """Returns list of dicts with fields name and MALID"""
+    now_date = datetime.datetime.now()
+    season_num = (now_date.date().month-1)//3
+    seasons = ['winter', 'spring', 'summer', 'fall']
+    season = seasons[season_num]+str(now_date.date().year)
+    raw = requests.get('https://www.senpai.moe/export.php?type=json&src='+season).json()
+    result = []
+    for item in raw['items']:
+        airdate_u = item['airdate_u']
+        if 'simulcast_airdate_u' in item:
+            airdate_u = item['simulcast_airdate_u']
+        name = item['name']
+        malid = item['MALID']
+        airdate = datetime.datetime.fromtimestamp(airdate_u)
+        if airdate.date() > now_date.date():
+            continue
+        if now_date.weekday() != airdate.weekday():
+            continue
+        anime_obj = {'name': name, 'MALID': malid}
+        result.append(anime_obj.copy())
+    return result
